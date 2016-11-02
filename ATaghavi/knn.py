8fbb7612cd3caf278861b915be2aea043fa0ed3f
Austin Taghavi <austintaghavi@gmail.com>
2016-11-02 09:28:32 -0500

    adding knn with proper i/o

--- ATaghavi/knn.py
+++ ATaghavi/knn.py
@@ -4,6 +4,7 @@ from PIL import Image
 import os
 import string
 import random
+import sys
 
 def getDistance(pixels1, pixels2):
 	dist = 0.0
@@ -14,53 +15,52 @@ def getDistance(pixels1, pixels2):
 
 def KNearestNeighbors(directoryName, K=3):
 	testfilenames = os.listdir(directoryName)
-
 	num = 0
 	errors = 0
 	K = 5
 
 	for filename in testfilenames:
-			if filename not in train_names:
-				im = Image.open("data/" + filename)
-				pixels = list(im.getdata())
-				for i in range(len(pixels)):
-					pixels[i] = pixels[i]*1.0/255.0
-				minDist = 99999.0
-				distances = []
-				for elem in data:
-					distance = getDistance(pixels, elem[0])
-					tup = (distance, elem[1])
-					distances.append(tup)
-				distances.sort(key=lambda tup: tup[0])
-				Knearest = distances[:K]
-				cnts = {}
-				for elem in Knearest:
-					if elem[1] not in cnts:
-						cnts[elem[1]] = 1
-					else:
-						cnts[elem[1]] += 1
-				expectedClass = 'A'
-				maxx = 0
-				for key in cnts:
-					if cnts[key] > maxx:
-						maxx = cnts[key]
-						expectedClass = key
-				num += 1
-				if expectedClass is not filename[:1]:
-					errors += 1
-				print filename + " -  " + expectedClass
+			im = Image.open(directoryName + "/" + filename)
+			pixels = list(im.getdata())
+			for i in range(len(pixels)):
+				pixels[i] = pixels[i]*1.0/255.0
+			minDist = 99999.0
+			distances = []
+			for elem in data:
+				distance = getDistance(pixels, elem[0])
+				tup = (distance, elem[1])
+				distances.append(tup)
+			distances.sort(key=lambda tup: tup[0])
+			Knearest = distances[:K]
+			cnts = {}
+			for elem in Knearest:
+				if elem[1] not in cnts:
+					cnts[elem[1]] = 1
+				else:
+					cnts[elem[1]] += 1
+			expectedClass = 'A'
+			maxx = 0
+			for key in cnts:
+				if cnts[key] > maxx:
+					maxx = cnts[key]
+					expectedClass = key
+			num += 1
+			if expectedClass is not filename[:1]:
+				errors += 1
+			print filename + "," + expectedClass
 	#print errors*1.0/num*1.0
 
-filenames = os.listdir("data")
+filenames = os.listdir("../Competition/data")
 data = []
 
-
+testDir = sys.argv[1]
+print testDir
 train_names = []
 
 for filename in filenames:
-	if random.random() < 0.9:
+	if random.random() < 1.0:
 		currentLetter = filename[:1]
-		im = Image.open("data/" + filename)
+		im = Image.open("../Competition/data/" + filename)
 		pixels = list(im.getdata())
 		for i in range(len(pixels)):
 			pixels[i] = pixels[i]*1.0/255.0
@@ -69,7 +69,5 @@ for filename in filenames:
 		train_names.append(filename)
 
 
-dirname = raw_input("Enter dir: ")
-
-KNearestNeighbors(dirname, 1)
+KNearestNeighbors(testDir, 1)
 
